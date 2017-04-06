1e927bf1754c8ae48feec8a26b62bdb780b25667
TOCHUKWU ORAJIAKU <tochukwuorajiaku@tochukwus-macbook-pro.local>
2017-04-06 15:13:57 -0600

    Initial Commit

---
+++ FactsGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FactsGame
+//
+//  Created by TOCHUKWU ORAJIAKU on 2017-04-06.
+//  Copyright © 2017 TOCHUKWU ORAJIAKU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
